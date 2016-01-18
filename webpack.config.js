59f103fd195e42f598ff818813139dae261d9b22
Mathew Glodack <glodack@gmail.com>
2016-01-18 15:10:37 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    'webpack/hot/only-dev-server',
+    'src/app.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      { test: /\.css$/, loader: "style!css" }
+  },
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ]
+}
